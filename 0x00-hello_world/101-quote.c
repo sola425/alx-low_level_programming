65419b4caaca525418fe67563a9ceb779022c87a
sola425 <solatinwo425@gmail.com>
2023-02-17 03:54:57 -0800

    step 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,19 @@
 #include <unistd.h>
+
 /**
  * main - Entry point
  *
  * Return: Always 1 (Error)
  */
-int main(void) {
-  char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-  write(STDERR_FILENO, msg, sizeof(msg)-1);
-  return 1;
+int main(void)
+{
+	char *msg = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	size_t len = 0;
+
+	while (*(msg + len))
+		len++;
+
+	if (write(STDERR_FILENO, msg, len) != len)
+		return (1);
+	return (1);
 }

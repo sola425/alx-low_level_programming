951c9f67c2dae4e3bee46490a8ae24a89748e1fa
sola425 <solatinwo425@gmail.com>
2023-02-16 22:24:01 -0800

    step 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
-
-int main() {
-    printf("with proper grammar, but the outcome is a piece of art,\n");
-    return 0;
+/**
+ * main - A program that prints a line using the printf function
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }

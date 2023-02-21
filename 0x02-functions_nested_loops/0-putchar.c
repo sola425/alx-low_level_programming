3c8f6a0e64c8deb23fa9b8b1013c0a75c0c9c37e
sola425 <solatinwo425@gmail.com>
2023-02-21 14:40:30 -0800

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,6 +1,5 @@
 #include "main.h"
 #include <stdio.h>
-
 /**
  * main - print Holberton as a message.
  *
@@ -10,7 +9,6 @@ int main(void)
 {
 	int str[] = {95, 112, 117, 116, 99, 104, 97, 114};
 	int count, sz;
-	
 	sz = sizeof(str) / sizeof(int);
 	for (count = 0; count < sz; count++)
 	{

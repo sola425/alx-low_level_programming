214c8b71663d1bb89df526fcc5f68a0d5083a750
sola425 <solatinwo425@gmail.com>
2023-02-21 14:20:14 -0800

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,22 @@
 #include "main.h"
+#include <stdio.h> // add this line to include the declaration of _putchar()
 
 /**
- * main - Prints Holberton as a message.
+ * main - print Holberton as a message.
  *
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-	int str[] = {95, 112, 117, 116, 99, 104, 97, 114};
-	int count, sz;
+        int str[] = {95, 112, 117, 116, 99, 104, 97, 114};
+        int count, sz;
 
-	sz = sizeof(str) / sizeof(int);
-	for (count = 0; count < sz; count++)
-	{
-		_putchar(str[count]);
-	}
-	_putchar('\n');
-	return (0);
+        sz = sizeof(str) / sizeof(int);
+        for (count = 0; count < sz; count++)
+        {
+                putchar(str[count]); // change _putchar to putchar
+        }
+        putchar('\n'); // change _putchar to putchar
+        return (0);
 }

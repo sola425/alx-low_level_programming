7e28042e267391e23e06d538c830805576e5e6bd
sola425 <solatinwo425@gmail.com>
2023-02-23 20:25:42 -0800

    task 12

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -3,33 +3,38 @@
 /**
  * print_number - prints an integer.
  * @n: input integer.
- * Return: no return.
+ *
+ * Description: This function takes an integer as input and prints it to the standard output.
+ * It works by converting the input integer to its absolute value, then iteratively dividing
+ * the value by 10 to obtain the digits of the number. Each digit is then printed to the standard
+ * output using the _putchar function.
  */
 void print_number(int n)
 {
-	unsigned int m, d, count;
+	unsigned int num, div = 1;
 
+	/* Handle negative numbers */
 	if (n < 0)
 	{
-		_putchar(45);
-		m = n * -1;
+		_putchar('-');
+		num = -n;
 	}
 	else
 	{
-		m = n;
+		num = n;
 	}
-
-	d = m;
-	count = 1;
-
-	while (d > 9)
+	
+	/* Determine the divisor needed to obtain the leftmost digit */
+	while (num / div >= 10)
 	{
-		d /= 10;
-		count *= 10;
+		div *= 10;
 	}
-
-	for (; count >= 1; count /= 10)
+	
+	/* Print each digit in the number */
+	while (div != 0)
 	{
-		_putchar(((m / count) % 10) + 48);
+		_putchar('0' + (num / div));
+		num %= div;
+		div /= 10;
 	}
 }
